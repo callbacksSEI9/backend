d934295de7bf4afe0bbfdc767189d394b59279ff
monano <mona.gaihab@gmail.com>
2020-01-30 16:43:11 +0300

    added the department model and department routes

---
+++ app/models/department.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const departmentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    owner:{//the manager of the department
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required: true
+    },
+    manager:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    employees:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required: true
+    }]
+
+})
+
+const DepartmentModel = mongoose.model("Department",departmentSchema)
+
+module.exports = DepartmentModel
